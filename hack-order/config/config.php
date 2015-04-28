3407c0efbc24a52c4c7347802b9b44ea012dda74
Gary Mazzone Peters <gary.m.peters@vanderbilt.edu>
2015-04-28 03:02:50 -0500

    First commit

---
+++ hack-order/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+//DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+//define("DB_NAME", "dobble");
+
+define("SITE_TITLE", "Welcome To Hack Order!");
